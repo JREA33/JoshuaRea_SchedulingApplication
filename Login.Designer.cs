3a10d1a0c24b4e254e7c47b50b0e75ae3430b2d8
Joshua Rea <Joshua Rea>
2023-08-09 13:26:44 +0000

    Setup MySql Connection

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace JoshuaRea_SchedulingApplication
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,34 @@ namespace JoshuaRea_SchedulingApplication
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(79, 53);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Login";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
     }
 }
 
---
+++ Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace JoshuaRea_SchedulingApplication
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //Get the connection string.
+            string constr = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+
+            //Make the connection.
+            MySqlConnection conn = null;
+
+            //Open the connection
+            try
+            {
+                conn = new MySqlConnection(constr);
+
+                //Open the connection
+                conn.Open();
+
+                MessageBox.Show("Connection is Open.");
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //Close the connection
+                if(conn!= null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}

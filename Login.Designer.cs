4b9a09dde2507b9bb71da8fb9c20f546c865f601
Joshua Rea <Joshua Rea>
2023-08-22 16:42:54 +0000

    Added Spanish support on Login and Populated Customer Grid

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -29,6 +29,7 @@ namespace JoshuaRea_SchedulingApplication
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
@@ -41,79 +42,49 @@ namespace JoshuaRea_SchedulingApplication
             // txtUserName
             // 
             this.txtUserName.BackColor = System.Drawing.SystemColors.Window;
-            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUserName.Location = new System.Drawing.Point(221, 169);
+            resources.ApplyResources(this.txtUserName, "txtUserName");
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(246, 29);
-            this.txtUserName.TabIndex = 0;
             // 
             // txtPassword
             // 
-            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(221, 220);
+            resources.ApplyResources(this.txtPassword, "txtPassword");
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(246, 29);
-            this.txtPassword.TabIndex = 1;
             // 
             // btnLogin
             // 
             this.btnLogin.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(234, 272);
+            resources.ApplyResources(this.btnLogin, "btnLogin");
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(96, 35);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(356, 272);
+            resources.ApplyResources(this.btnExit, "btnExit");
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(96, 35);
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // lblUsername
             // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(44, 171);
+            resources.ApplyResources(this.lblUsername, "lblUsername");
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(110, 25);
-            this.lblUsername.TabIndex = 4;
-            this.lblUsername.Text = "Username";
             // 
             // lblPassword
             // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(44, 222);
+            resources.ApplyResources(this.lblPassword, "lblPassword");
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(106, 25);
-            this.lblPassword.TabIndex = 5;
-            this.lblPassword.Text = "Password";
             // 
             // lblLogin
             // 
-            this.lblLogin.AutoSize = true;
-            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.Location = new System.Drawing.Point(191, 64);
+            resources.ApplyResources(this.lblLogin, "lblLogin");
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(106, 39);
-            this.lblLogin.TabIndex = 6;
-            this.lblLogin.Text = "Login";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(530, 405);
             this.Controls.Add(this.lblLogin);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUsername);
@@ -122,7 +93,6 @@ namespace JoshuaRea_SchedulingApplication
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUserName);
             this.Name = "Login";
-            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Login.cs
+++ Login.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,23 +16,41 @@ namespace JoshuaRea_SchedulingApplication
 {
     public partial class Login : Form
     {
+        string loginError = "Username and Password do not match.";
         public Login()
         {
             InitializeComponent();
             MySqlConnection c = DBConnection.conn;
+
+            setLanguage();
         }
 
+        private void setLanguage()
+        {
+            if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "es")
+            {
+                lblLogin.Text = "Acceso";
+                lblUsername.Text = "Nombre de Usuario";
+                lblPassword.Text = "Contrasena";
+                btnLogin.Text = "Acceso";
+                btnExit.Text = "Salida";
+                this.Text = "Inicio de Sesion del Programador";
+                loginError = "Nombre de usuario y contrasena no coincidem.";
+            }
+        }
         private void btnLogin_Click(object sender, EventArgs e)
         {
             bool foundUser = User.FindUser(txtUserName.Text, txtPassword.Text);
 
             if (foundUser == true)
             {
-
+                this.Hide();
+                new Main().ShowDialog();
+                this.Close();
             }
             else
             {
-                MessageBox.Show("Username and Password do not match.");
+            MessageBox.Show(loginError);
             }
         }
 
